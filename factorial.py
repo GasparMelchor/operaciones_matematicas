4823f4cd10ee6417b93cc9e87c13e9e45be689c3
Gaspar Melchor Caamal Aban <gaspar.caamalaban@itsva.edu.mx>
2021-12-12 12:47:31 -0600

    se agrego calculo de porcentajes y calculo de hipotenusa

--- factorial.py
+++ factorial.py
@@ -1,6 +1,9 @@
 from math import factorial
+import math
 
 print("que operacion desdea hacer \n1: suma\n2: resta\n3: mutiplicacion\n4: division\n5: factorial")
+print("nuevas operaciones \n6: calculo de porcentaje\n7: calculo de hipotenusa")
+
 opcion=int(input())
 if(opcion==1):
     print("ingrese los numeros a sumar")
@@ -31,3 +34,19 @@ if(opcion==5):
     print("que numero desea que calculen el factorial")
     numero=int(input())
     print("la division da como resultado: ",factorial(numero))
+
+if(opcion==6):
+    print("ingrese los numeros para el calculo de porcentaje el primero debe ser el el 100")
+    numero=int(input())
+    numero1=int(input())
+    
+    print("el valor porcentual es ", ((100*numero1)/numero))
+
+if(opcion==7):
+    print("ingrese la medida del cateto opuesto y del cateto adyacente")
+    numero=int(input())
+    numero1=int(input())
+    numero=numero**2
+    numero1=numero1**2
+    total=math.sqrt(numero+numero1)
+    print("la division da como resultado: ",total)
