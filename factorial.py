b499b20308594d9e8d38c7c1ec451c138ff40966
Gaspar Melchor Caamal Aban <gaspar.caamalaban@itsva.edu.mx>
2021-12-11 22:17:15 -0600

    se agrego suma de 2 numeros

--- factorial.py
+++ factorial.py
@@ -2,3 +2,9 @@ from math import factorial
 print("factorial del numero 9 es")
 print(factorial(9))
 
+print('ingrese 2 numeros para ser sumados')
+num1=input()
+num2=input()
+num1=int(num1)
+num2=int(num2)
+print('la suma es ',(num1+num2))
